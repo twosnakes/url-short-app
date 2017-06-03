ba3fb39e894052443761b642b76c32d0209c14a3
Zoe Lembeck <zlembeck@gmail.com>
2017-06-03 08:40:29 -0500

    added routes and two homepages for users logged in and out

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+
+  devise_scope :user do
+   authenticated :user do
+     root 'links#index', as: :authenticated_root
+   end
+
+   unauthenticated do
+     root 'devise/registrations#new', as: :unauthenticated_root
+   end
+ end
+
+get '/' => 'slugs#index' 
+get '/slugs' => 'slugs#index'
+
+get '/slugs/new' => 'slugs#new'
+post '/slugs' => 'slugs#create'
+
+get '/slugs/:id' => 'slugs#show'
+
+get '/slugs/:id/edit' => 'slugs#edit'
+patch '/slugs/:id' => 'slugs#update'
+
+delete '/slugs/:id' => 'slugs#destroy'
+
+ 
 end
